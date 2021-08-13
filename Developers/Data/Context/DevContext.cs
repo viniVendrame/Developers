a3249ab137221bbe521031f42e6fe7267199a9d0
Vinicius Vendrame <vini.vendrame13@gmail.com>
2021-08-13 16:09:05 -0300

    Table Developers Created and changing DevContext class to Public Closed #29

--- Developers/Data/Context/DevContext.cs
+++ Developers/Data/Context/DevContext.cs
@@ -8,12 +8,13 @@ using System.Threading.Tasks;
 
 namespace Data.Context
 {
-    class DevContext : DbContext
+    public class DevContext : DbContext
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Developer> Developers { get; set; }
 
         public DevContext() : base("Data Source=192.168.0.117;Initial Catalog=Developers;Persist Security Info=True;User ID=Developers;Password=@devlabs1")
         {
