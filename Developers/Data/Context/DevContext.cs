302b3a3aeb7eb78dc6a23af14b6efc517c30db52
Vinicius Vendrame <vini.vendrame13@gmail.com>
2021-08-13 15:10:06 -0300

    Table Addresses created Closed #26

--- Developers/Data/Context/DevContext.cs
+++ Developers/Data/Context/DevContext.cs
@@ -12,6 +12,7 @@ namespace Data.Context
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Address> Addresses { get; set; }
 
         public DevContext() : base("Data Source=192.168.0.117;Initial Catalog=Developers;Persist Security Info=True;User ID=Developers;Password=@devlabs1")
         {
---
+++ Developers/Data/Migrations/202108131809056_CreateTableAddresses.cs
@@ -0,0 +1,30 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableAddresses : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Addresses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Street = c.String(),
+                        Number = c.Int(nullable: false),
+                        Complement = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Addresses");
+        }
+    }
+}
