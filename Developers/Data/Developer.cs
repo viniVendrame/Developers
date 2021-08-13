65e49e2673c7c9e7e97b467131459dab045d688f
caio <caio.mateus.r@gmail.com>
2021-08-13 13:50:45 -0300

    Developer model created

---
+++ Developers/Data/Developer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class Developer : Base
+    {
+        public User User { get; set; }
+        public Profile Profile { get; set; }
+        public Contact Contact { get; set; }
+        public Address Address { get; set; }
+    }
+}
